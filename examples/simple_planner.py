ebc06a2a140d29c4246de9bd99f15a651a313dc6
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-11-01 12:52:35 +0100

    Improved the example code
    Added experimenting on RouteSet and working / sorting on each route

--- examples/simple_planner.py
+++ examples/simple_planner.py
@@ -6,7 +6,6 @@ cwd_parent = os.path.abspath(os.path.join(cwd, os.pardir))
 sys.path.append(cwd)
 sys.path.append(cwd_parent)
 
-
 import emission
 
 
@@ -19,7 +18,34 @@ if __name__ == '__main__':
     vehicle = emission.vehicles.Truck(fuel_diesel)
 
 
+
     planner = emission.Planner(start, stop, vehicle)
     planner.add_pollutant(emission.PollutantTypes.NOx)
     planner.add_pollutant(emission.PollutantTypes.CO)
     planner.run()
+
+    routes = planner.routes
+
+    print("Pollutants: {}".format(planner.pollutants.keys()))
+    pollutant_types = planner.pollutants.keys()
+    for r in routes:
+        print("Route: {}".format(r))
+        for pt in pollutant_types:
+            print("    {} = {}".format(pt, r.total_emission(pt)))
+
+
+    print("\nTesting below....:")
+    routes.sort()
+    print("After sorted..:")
+    for r in routes:
+        print(">> {}".format(r))
+
+    sorted_after_NOx = sorted(routes, key=lambda x: x.total_emission('NOx'))
+    print("Sorted after NOx..:")
+    for r in sorted_after_NOx:
+        print(">> {} NOx: {}".format(r, r.total_emission('NOx')))
+
+    sorted_after_CO = sorted(routes, key=lambda x: x.total_emission('CO'))
+    print("Sorted after CO")
+    for r in sorted_after_CO:
+        print(">> {} CO: {}".format(r, r.total_emission('CO')))
